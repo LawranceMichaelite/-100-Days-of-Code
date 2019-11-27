5364ed5e23de6794dc151ea671e177b0bdbeae3d
jeyalawrance@gmail.com <jeyalawrance@gmail.com>
2019-11-27 22:24:48 +0530

    Day-07 of 100 Days of Code , A Terrible Day with 0 Lines Of Code

---
+++ Day-07/Idiot.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "What a Terrible Day" << endl;
+    return 0;
+}
---
+++ Day-07/Learning-CPP-Vector.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> array_1 = {
+        1,
+        2,
+        3,
+        4,
+        5};
+    cout << array_1[0] << endl;
+    return 0;
+}
