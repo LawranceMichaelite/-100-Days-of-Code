291d6f6d3b7b93d040e4ec8627851b96c8649f81
jeyalawrance@gmail.com <jeyalawrance@gmail.com>
2019-12-12 12:52:26 +0530

    Day-21 of 100 Days of Code, Nothing to Say😑

---
+++ Day-21/app/src/main/java/com/example/my_application/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.my_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void clickFunc(View view) {
+        Log.i("Info : ", "You Clicked that button Dude");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
